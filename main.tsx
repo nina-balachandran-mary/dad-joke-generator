d515962b5520429a0282d2606ea3ae8926ca02b4
nina-balachandran-mary <nina.balachandranmary@gmail.com>
2024-09-15 01:07:26 -0500

    moving main tsx to root folder

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
-import App from './components/App.tsx'
+import App from './src/components/App.tsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
 
